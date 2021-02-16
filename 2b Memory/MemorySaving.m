f64900c56bd1e9707f3455c0298c3596f5879fe9
saad iftikhar <daasdemha@hotmail.com>
2021-02-16 20:14:51 +0000

    Made a Loadalldata function

--- 2b Memory/MemorySaving.m
+++ 2b Memory/MemorySaving.m
@@ -6,7 +6,7 @@ FileName = '..\Model\o3_surface_20180701000000.nc'; % define the name of the fil
 
 Contents = ncinfo(FileName); % Store the file content information in a variable.
 
-Loadalldata
+[AllDataMem] = Loadalldata(FileName);
 
 LoadHours
 
