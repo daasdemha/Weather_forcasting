8ebe451ce26caf71d85841b8185f694d5b635848
saad iftikhar <daasdemha@hotmail.com>
2021-03-19 23:10:11 +0000

    Added automated tests

--- 6a Testing Text/TestText.m
+++ 6a Testing Text/TestText.m
@@ -1,14 +1,13 @@
 %% Script to examine NetCDF data formats and check for non-numeric values (chars only)
 
-clear all
-close all
+%% Data to test 
+function FindText = TestText(FileName)% define our test file
 
 %% Define plain text variable types
 DataTypes = {'NC_Byte', 'NC_Char', 'NC_Short', 'NC_Int', 'NC_Float', 'NC_Double'};
 
 %% Test a good file
-%% Set file to test
-FileName = '../Model/o3_surface_20180701000000.nc'; % define our test file
+%% Set file to tes
 
 Contents = ncinfo(FileName); % Store the file content information in a variable.
 FileID = netcdf.open(FileName,'NC_NOWRITE'); % open file read only and create handle
@@ -24,39 +23,19 @@ DataInFile = DataTypes(datatype)'
 %% find character data types
 FindText = strcmp('NC_Char', DataInFile);
 
+%% Create and open log file
+% create new log file, 'w' replaces the file if present. To continually
+% append, use 'a'
+LogID = fopen('AnalysisLog.txt', 'w');
 %% print results
 fprintf('Testing file: %s\n', FileName)
 if any(FindText)
     fprintf('Error, text variables present:\n')
+   % Write to log file
+    fprintf(LogID, '%s text variables present in \n', FileName);
 else
     fprintf('All data is numeric, continue analysis.\n')
+    % Write to log file
+    fprintf(LogID, '%s All data is numeric in \n', FileName);
 end
-
-%% #####
-
-%% Test File with Errors
-%% Set file to test
-    FileName = '../Model/TestFileText.nc'; % define our test file
-
-    Contents = ncinfo(FileName); % Store the file content information in a variable.
-    FileID = netcdf.open(FileName,'NC_NOWRITE'); % open file read only and create handle
-
-    for idx = 0:size(Contents.Variables,2)-1 % loop through each variable
-        % read data type for each variable and store
-        [~, datatype(idx+1), ~, ~] = netcdf.inqVar(FileID,idx);
-    end
-
-    %% display data types
-    DataInFile = DataTypes(datatype)'
-
-    %% find character data types
-    FindText = strcmp('NC_Char', DataInFile);
-
-    %% print results
-    fprintf('Testing file: %s\n', FileName)
-    if any(FindText)
-        fprintf('Error, text variables present:\n')
-    else
-        fprintf('All data is numeric, continue analysis.\n')
-    end
-
+end
