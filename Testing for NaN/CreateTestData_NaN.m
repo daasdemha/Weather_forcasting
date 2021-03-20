2e1e0e0dc91eb7f2845713c650fff7af0696270d
Saad Iftikhar (iftikhars) <iftikhars@coventry.ac.uk>
2021-03-20 10:27:21 +0000

    Tests for NaN
    catches if NaN has been placed instead of text

--- Testing for NaN/CreateTestData_NaN.m
+++ Testing for NaN/CreateTestData_NaN.m
@@ -1,20 +1,62 @@
-%% Replaces one hours worth of data with NaN
+%% Script to examine NetCDF data formats and check for NaN
+% Note, you would carry out this test each time you load data.
+% You should NOT test the whole file at the start
+
 clear all
 close all
 
-OriginalFileName = './Model/o3_surface_20180701000000.nc';
-NewFileName = './Model/TestFileNaN.nc';
-copyfile(OriginalFileName, NewFileName);
 
-C = ncinfo(NewFileName);
-ModelNames = {C.Variables(1:8).Name};
+%% Test File with Errors
+NaNErrors = 0;
+%% Set file to test
+TestArr = {'../Model/TestFileText.nc', '../Model/o3_surface_20180701000000.nc'};
+for FileName = TestArr
+DataFileName = FileName{1};
+
+%DataFileName = '../Model/TestFileNaN.nc'; % define our test file
+% FileName = '../Model/o3_surface_20180701000000.nc'; % un rem this line to see what happens with good data
+Contents = ncinfo(DataFileName); % Store the file content information in a variable.
 
 
-%% Change data to NaN
-BadData = NaN(700,400,1);
 
-%% Write to *.nc file
-Hour2Replace = 12;
-for idx = 1:8
-    ncwrite(NewFileName, ModelNames{idx}, BadData, [1, 1, Hour2Replace]);
+% create new log file, 'w' replaces the file if present. To continually
+% append, use 'a'
+LogID = fopen('AnalysisLog.txt', 'a');
+fprintf(LogID, '%s: Starting analysis of %s\n', datestr(now, 0), DataFileName);
+
+StartLat = 1;
+StartLon = 1;
+
+fprintf('Testing files: %s\n', DataFileName)
+for idxHour = 1:25
+    
+    for idxModel = 1:8
+        Data(idxModel,:,:) = ncread(DataFileName, Contents.Variables(idxModel).Name,...
+            [StartLat, StartLon, idxHour], [inf, inf, 1]); % 'inf' reads all the data
+    end
+    
+    % check for NaNs
+    if any(isnan(Data), 'All')
+        NaNErrors = 1;
+        %% display warning
+        fprintf('NaNs present\n')
+        ErrorModel = find(isnan(Data), 1, 'first');
+        %% find first error:
+        fprintf('Analysis for hour %i is invalid, NaN errors recorded in model %s\n',...
+            idxHour, Contents.Variables(ErrorModel).Name)
+        
+        % Write to log file
+        fprintf(LogID, '%s: %s processing data hour %i\n', datestr(now, 0), 'NaN Error', idxHour);
+    else
+        % write to log file
+        fprintf(LogID, '%s: %s processing data hour %i\n', datestr(now, 0), 'Success', idxHour);
+    end
+    
+end
+fprintf(LogID,'\n');
+fclose(LogID);
+
+if ~NaNErrors
+    fprintf('No errors!\n')
+end
 end
