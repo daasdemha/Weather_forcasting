5ce587fb14250439a8d8b8aa65ee8e7cc0998911
Saad Iftikhar (iftikhars) <iftikhars@coventry.ac.uk>
2021-04-04 18:00:07 +0100

    Update ManualGraphs.m

--- Sequential And Parallel Processing/ManualGraphs.m
+++ Sequential And Parallel Processing/ManualGraphs.m
@@ -1,24 +1,20 @@
 %% Plotting graphs in Matlab
-clear all
-close all
-%if the user would like to plot graphs on a later date they can by entring
-%the values of time of processors and time taken to complete the task.
-%in the x1Vals and y1Vals below
 %% Show two plots on different y-axes
-%% 250 data processed
-x1Vals = [1, 2, 3, 4, 5, 6]; %Number of processors used
-y1Vals = [95.52, 39.00, 47, 44, 40, 39];%The Time taken
+%% 10,000 data processed
+x1Vals = [1, 2, 3, 4, 5, 6, 7, 8];
+y1Vals = [122, 64, 49, 44, 37, 38, 33, 34];
 figure(1)
 yyaxis left
 plot(x1Vals, y1Vals, '-bd')
+grid on; 
 xlabel('Number of Processors')
 ylabel('Processing time (s)')
 title('Processing time vs number of processors')
 
 
-%% 5,000 data processed
-x2Vals = [2, 3, 4, 5, 6, 7, 8];
-y2Vals = [1560, 1077, 945, 838, 852, 725, 707];
+%% 69,451 data processed
+x2Vals = [1, 2, 3, 4, 5, 6, 7, 8];
+y2Vals = [925, 510, 354, 270, 245, 236, 221, 212];
 figure(1)
 yyaxis right
 plot(x2Vals, y2Vals, '-rx')
@@ -26,19 +22,38 @@ xlabel('Number of Processors')
 ylabel('Processing time (s)')
 title('Processing time vs number of processors')
 
-legend('250 Data', '5,000 Data')
+legend('10,000 Data', '69,451 Data')
 
 
 %% Show two plots on same y-axis
 %% Mean processing time
-y1MeanVals = y1Vals / 250;
-y2MeanVals = y2Vals / 5000;
+y1MeanVals = y1Vals / 69451;
+y2MeanVals = y2Vals / 10000;
 
 figure(2)
 plot(x1Vals, y1MeanVals, '-bd')
+grid on; 
 hold on
 plot(x2Vals, y2MeanVals, '-rx')
 xlabel('Number of Processors')
 ylabel('Processing time (s)')
 title('Mean Processing time vs number of processors')
-legend('250 Data', '5,000 Data')
+legend('10,000 Data', '69,451 Data')
+
+figure(3)
+Poly = polyfit(x2Vals,y2Vals,2); 
+Poly2 = polyval(Poly,x2Vals);
+plot(x2Vals, y2Vals, x2Vals, Poly2)
+ colors = [0.8157 0.1255 0.5647  % Adds RGB colours
+              0.1843 0.3098 0.3098
+               ];
+colororder(colors)  % The Order for the colours is decided
+plot(x2Vals,y2Vals,x2Vals, Poly2, 'lineWidth',2); % plots the data 
+grid on; 
+hold on
+title("Processing time vs number of processors"); 
+legend('Location','northwest'); 
+
+xlabel('Number of Processors'); 
+ylabel('Processing time (s)'); 
+legend('Our values','Average'); 
